95aa6ae99a1e9c880fbe12f69f21ba1e7d06a6cd
DarccMan <darcy3057@gmail.com>
2021-03-24 17:37:44 +1100

    fix

--- assets/js/jquery.li-scroller.1.0.js
+++ assets/js/jquery.li-scroller.1.0.js
@@ -1,7 +1,7 @@
 /*!
  * liScroll 1.0
  * Examples and documentation at: 
- * http://www.gcmingati.net/wordpress/wp-content/lab/jquery/newsticker/jq-liscroll/scrollanimate.html
+ * http://www.gcmingati.net/wordpress/wp-content/lab/jquery/news/ticker/jq-liscroll/scrollanimate.html
  * 2007-2010 Gian Carlo Mingati
  * Version: 1.0.2.1 (22-APRIL-2011)
  * Dual licensed under the MIT and GPL licenses:
@@ -13,35 +13,35 @@
  */
 
 
-jQuery.fn.liScroll = function(settings) {
-		settings = jQuery.extend({
+jQuery.fn.liScroll = function (settings) {
+	settings = jQuery.extend({
 		travelocity: 0.07
-		}, settings);		
-		return this.each(function(){
-				var $strip = jQuery(this);
-				$strip.addClass("newsticker")
-				var stripWidth = 1;
-				$strip.find("li").each(function(i){
-				stripWidth += jQuery(this, i).outerWidth(true); // thanks to Michael Haszprunar and Fabien Volpi
-				});
-				var $mask = $strip.wrap("<div class='mask'></div>");
-				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
-				var containerWidth = $strip.parent().parent().width();	//a.k.a. 'mask' width 	
-				$strip.width(stripWidth);			
-				var totalTravel = stripWidth;
-				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
-				function scrollnews(spazio, tempo){
-				$strip.animate({left: '-='+ spazio}, tempo, "linear", function(){$strip.css("left", containerWidth); scrollnews(totalTravel, defTiming);});
-				}
-				scrollnews(totalTravel, defTiming);				
-				$strip.hover(function(){
-				jQuery(this).stop();
-				},
-				function(){
+	}, settings);
+	return this.each(function () {
+		var $strip = jQuery(this);
+		$strip.addClass("news/ticker")
+		var stripWidth = 1;
+		$strip.find("li").each(function (i) {
+			stripWidth += jQuery(this, i).outerWidth(true); // thanks to Michael Haszprunar and Fabien Volpi
+		});
+		var $mask = $strip.wrap("<div class='mask'></div>");
+		var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");
+		var containerWidth = $strip.parent().parent().width();	//a.k.a. 'mask' width 	
+		$strip.width(stripWidth);
+		var totalTravel = stripWidth;
+		var defTiming = totalTravel / settings.travelocity;	// thanks to Scott Waye		
+		function scrollnews(spazio, tempo) {
+			$strip.animate({left: '-=' + spazio}, tempo, "linear", function () {$strip.css("left", containerWidth); scrollnews(totalTravel, defTiming);});
+		}
+		scrollnews(totalTravel, defTiming);
+		$strip.hover(function () {
+			jQuery(this).stop();
+		},
+			function () {
 				var offset = jQuery(this).offset();
 				var residualSpace = offset.left + stripWidth;
-				var residualTime = residualSpace/settings.travelocity;
+				var residualTime = residualSpace / settings.travelocity;
 				scrollnews(residualSpace, residualTime);
-				});			
-		});	
+			});
+	});
 };
